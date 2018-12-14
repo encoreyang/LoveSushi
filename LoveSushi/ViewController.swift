98fb88771a1a245546392f2700a3714a276e8ccb
Terrence Yang <terrence@2001-b400-e35f-a83d-04a5-f29d-02e9-ce40.emome-ip6.hinet.net>
2018-12-14 14:46:13 +0800

    Initial Commit

---
+++ LoveSushi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoveSushi
+//
+//  Created by terrence on 2018/12/14.
+//  Copyright © 2018 terrence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
